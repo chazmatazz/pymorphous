f595ff5143bac3acb584312cc7a9d50010c6cb71
Charles Dietrich <charles.m.dietrich@gmail.com>
2010-11-13 12:53:48 -0700

    Switched to pyglet. Graphics not working. Testing backtrace speed.

--- pymorphous/__init__.py
+++ pymorphous/__init__.py
@@ -1,16 +1,9 @@
 import random
 import numpy
 import math
-from PySFML import sf
+import pyglet
+from pyglet.gl import *
 import inspect
-
-id_counter = 0
-
-def getid():
-    global id_counter
-    id = id_counter
-    id_counter += 1
-    return id
 
 class NbrKeyError(Exception):
     def __init__(self, value):
@@ -19,11 +12,10 @@ class NbrKeyError(Exception):
         return repr(self.value)
     
 class BaseDevice(object):
-    def __init__(self, pos, id, step_size, radio_range, cloud):
+    def __init__(self, pos, id, radio_range, cloud):
         self._pos = pos
         self.id = id
-        self.step_size = step_size
-        self.radio_range = radio_range
+        self._radio_range = radio_range
         self.cloud = cloud
         self.leds = [0, 0, 0]
         self.senses = [0, 0, 0]
@@ -31,8 +23,17 @@ class BaseDevice(object):
         self._vals = {}
         self._keys = set()
     
+    @property
+    def radio_range(self):
+        return self._radio_range
+    
+    @radio_range.setter
+    def radio_range(self, value):
+        self.cloud.connectivity_changed = True
+        self._radio_range = value
+        
     def move(self, vector):
-        self.cloud.moved = True
+        self.cloud.connectivity_changed = True
         self._pos += vector
     
     @property
@@ -41,7 +42,7 @@ class BaseDevice(object):
     
     @x.setter
     def x(self, value):
-        self.cloud.moved = True
+        self.cloud.connectivity_changed = True
         self._pos[0] = value
 
     @property
@@ -50,7 +51,7 @@ class BaseDevice(object):
     
     @y.setter
     def y(self, value):
-        self.cloud.moved = True
+        self.cloud.connectivity_changed = True
         self._pos[1] = value
         
     @property
@@ -59,7 +60,7 @@ class BaseDevice(object):
     
     @z.setter
     def z(self, value):
-        self.cloud.moved = True
+        self.cloud.connectivity_changed = True
         self._pos[2] = value  
     
     @property
@@ -68,7 +69,7 @@ class BaseDevice(object):
     
     @pos.setter
     def pos(self, new_pos):
-        self.cloud.moved = True
+        self.cloud.connectivity_changed = True
         self._pos = new_pos
     
     def nbr(self, val, extra_key=None):
@@ -76,9 +77,8 @@ class BaseDevice(object):
         return self._nbr("%s%s" % (key, extra_key), val)
     
     def _stack_location(self):
-        # file:line_number:index_of_last_atttempted_instruction_in_bytecode
-        # the last attempted instruction in bytecode is not safe
-        return repr(["%s:%d:%d" % (f[1], f[2], f[0].f_lasti) for f in inspect.stack()])
+        # file:line_number
+        return repr(["%s:%d" % (f[1], f[2]) for f in inspect.stack(context=0)])
 
     def _nbr(self, key, val):
         if key in self._keys:
@@ -111,47 +111,74 @@ class BaseDevice(object):
     
     @property
     def dt(self):
-        return 0.01 if self.step_size==0 else self.step_size
+        return self._dt
         
-    def _step(self):
+    def _step(self, dt):
+        self._dt = dt
         self.step()
         
     def _advance(self):
         self._keys = set()
-    
-    def _draw(self, window):
-        window.Draw(sf.Shape.Circle(self.pos[0]*1000, 
-                                    self.pos[1]*1000,
-                                    10,
-                                    sf.Color(255,255,255,255)))
-        window.Draw(sf.Shape.Circle(self.pos[0]*1000, 
-                                    self.pos[1]*1000,
-                                    0.1*1000,
-                                    sf.Color(255,0,0,100)))
-        text = sf.String(repr(self.leds))
-        text.SetSize(20)
-        text.Move(self.pos[0]*1000, self.pos[1]*1000)
-        window.Draw(text)
-
+        
     def __repr__(self):
         return "id: %s, leds: %s, senses: %s, pos: %s" % (
                         self.id, repr(self.leds), repr(self.senses), 
                         repr(self.pos))
-        
+      
+class DefaultSkin(object):
+    def setup(self):
+        # One-time GL setup
+        glClearColor(1, 1, 1, 1)
+        glColor3f(1, 0, 0)
+        glEnable(GL_DEPTH_TEST)
+        glEnable(GL_CULL_FACE)
+    
+        # Uncomment this line for a wireframe view
+        #glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+    
+        # Simple light setup.  On Windows GL_LIGHT0 is enabled by default,
+        # but this is not the case on Linux or Mac, so remember to always 
+        # include it.
+        glEnable(GL_LIGHTING)
+        glEnable(GL_LIGHT0)
+        glEnable(GL_LIGHT1)
+    
+        # Define a simple function to create ctypes arrays of floats:
+        def vec(*args):
+            return (GLfloat * len(args))(*args)
+    
+        glLightfv(GL_LIGHT0, GL_POSITION, vec(.5, .5, 1, 0))
+        glLightfv(GL_LIGHT0, GL_SPECULAR, vec(.5, .5, 1, 1))
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, vec(1, 1, 1, 1))
+        glLightfv(GL_LIGHT1, GL_POSITION, vec(1, 0, .5, 0))
+        glLightfv(GL_LIGHT1, GL_DIFFUSE, vec(.5, .5, .5, 1))
+        glLightfv(GL_LIGHT1, GL_SPECULAR, vec(1, 1, 1, 1))
+    
+        glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, vec(0.5, 0, 0.3, 1))
+        glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, vec(1, 1, 1, 1))
+        glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 50)
+
+
+    
+    def body(self, device, batch, group):
+        pass
+    
+    def state(self, device, batch, group):
+        pass
+    
+    def metrics(self, dt, batch, group):
+        pass
+
 class spawn_cloud(object):      
     def __init__(self, klass=None, args=None, num_devices=None, devices=None, 
-                step_size=0, radio_range=0.05, width=1000, height=1000, 
-                window_title=None, _3D=False):
-    
-        window = sf.RenderWindow(sf.VideoMode(width, height), 
-                                 window_title if window_title else klass.__name__)
+                step_size = 0, radio_range=0.05, width=1000, height=1000, 
+                window_title=None, _3D=False, skin=DefaultSkin()):
         
         if not devices:
             devices = []
             for i in range(0, num_devices):
                 d = klass(pos = numpy.array([random.random(), random.random(), random.random() if _3D else 0]),
-                          id = getid(), 
-                          step_size = step_size, 
+                          id = i,
                           radio_range = radio_range, 
                           cloud = self)
                 devices += [d]
@@ -162,37 +189,48 @@ class spawn_cloud(object):
                         d.setup()
                 
         
-        self.moved = True
-            
-        running = True
-        while running:
-            event = sf.Event()
-            while window.GetEvent(event):
-                if event.Type == sf.Event.Closed:
-                    running = False
-                
-            if self.moved:
+        self.connectivity_changed = True
+        
+        window = pyglet.window.Window()
+        batch = pyglet.graphics.Batch()
+        body_group = pyglet.graphics.OrderedGroup(0)
+        state_group = pyglet.graphics.OrderedGroup(1)
+        metrics_group = pyglet.graphics.OrderedGroup(2)
+        skin.setup()
+        
+        dts = [[]]
+        def update(dt):
+            dts[0] += [dt]
+            _dts = dts[0][2:]
+            if(len(_dts) == 10):
+                print "dts=%s, average=%f" % (_dts, sum(_dts)/len(_dts))
+                exit()
+            if self.connectivity_changed:
                 for d in devices:
                     d._nbrs = []
                     for e in devices:
                         if d != e:
                             delta = e.pos - d.pos
-                            dist = math.sqrt(numpy.dot(delta, delta))
-                            if dist < d.radio_range:
+                            if numpy.dot(delta, delta) < d.radio_range * d.radio_range:
                                 d._nbrs += [e]
+                for d in devices:
+                    skin.body(device=d, batch=batch, group=body_group)
+            self.connectivity_changed = False
             
-            self.moved = False
-            
+            skin.metrics(dt=dt, batch=batch, group=metrics_group)
             for d in devices:
-                d._step()
-            
+                skin.state(device=d, batch=batch, group=state_group)
+            for d in devices:
+                d._step(dt)
             for d in devices:
                 d._advance()
                 
-            window.Clear()
-            for d in devices:
-                d._draw(window)
-            window.Display()
+        pyglet.clock.schedule_interval(update, 1/100. if step_size==0 else step_size)
+                
+        @window.event
+        def on_draw():
+            batch.draw()
             
+        pyglet.app.run()
             
 from pymorphous.lib import *
